f718449b997cb0d50d937bfb4cc3c924d5e3c8ee
Mi Min <mimin@Mis-MacBook-Pro.local>
2022-11-16 21:18:51 +0700

    update

--- tests/testDefault.test.js
+++ tests/testDefault.test.js
@@ -1,6 +1,6 @@
 require("chromedriver");
 
-const {Builder, By, Key} = require("selenium-webdriver");
+const {Builder, By, Key, until, error} = require("selenium-webdriver");
 const assert = require("chai").assert;
 
 //describe - describes test
@@ -53,19 +53,63 @@ describe("Default test plan", function () {
             await driver.quit();
         }
     })
-    it("send mail",async function(){
+    // it("send mail",async function(){
+    //     const driver = await new Builder().forBrowser("chrome").build();
+    //
+    //     try{
+    //         await driver.get("https://namnguyen.io/");
+    //         // await driver
+    //         //     .findElement(By.xpath('//input[@name="firstName"]')).sendKeys("Min");
+    //         // await driver
+    //         //     .findElement(By.name('lastName')).sendKeys('kute');
+    //         // await driver
+    //         //     .findElement(By.name('email')).sendKeys('mimin110191@gmail.com');
+    //         // await driver
+    //         //     .findElement(By.name('message')).sendKeys('Mập địt tới đây');
+    //
+    //         await Promise.all([
+    //             driver.findElement(By.xpath('//input[@name="firstName"]')).sendKeys("Min"),
+    //             driver.findElement(By.name('lastName')).sendKeys('kute'),
+    //             driver.findElement(By.name('email')).sendKeys('mimin110191@gmail.com'),
+    //             driver.findElement(By.name('message')).sendKeys('Mập địt tới đây')
+    //         ]);
+    //         await driver
+    //             .findElement(By.xpath("//button[text()='Send Message']")).click();
+    //
+    //         // const a = new Promise((resolve, reject) => {
+    //         //     return reject('a');
+    //         // }).then(result => console.log(result)).catch(error => console.log(error));
+    //
+    //         const text = await driver.wait(until.elementLocated(By.id('headlessui-dialog-title-7')), 5000);
+    //         assert.equal(await text.getAttribute("innerHTML") ,'Notification');
+    //             // .then(el => assert.equal(el.getAttribute("innerHTML") ,'Notification'))
+    //             // .catch(error => console.log(error));
+    //     }
+    //     finally {
+    //         await driver.quit();
+    //     }
+    // })
+    it(' validation send mail',async function(){
         const driver = await new Builder().forBrowser("chrome").build();
-
         try{
-            await driver.get("https://namnguyen.io/blog");
-            await driver
-                .findElement(By.xpath("//form/div[1]/input"))
-                .sendKeys("Min");
-            await driver
-                .findElement(By.xpath('("//button[text()='Send Message'']")).click()
-        }
-        finally {
-            await driver.quit();
+            await driver.get("https://namnguyen.io/");
+            await driver.findElement(By.xpath("//button[text()='Send Message']")).click();
+
+            const [firstName, lastName, email, message] = await Promise.all([
+                driver.wait(until.elementsLocated(By.xpath("//input[@name='firstName']/following-sibling::span[1]")), 1000),
+                driver.wait(until.elementsLocated(By.xpath("//input[@name='lastName']/following-sibling::span[1]")),1000),
+                driver.wait(until.elementsLocated(By.xpath("//input[@name='email']/following-sibling::span[1]")),1000),
+                driver.wait(until.elementsLocated(By.xpath("//textarea[@name='message']/following-sibling::span[1]")),1000)
+            ])
+
+            assert.equal(await firstName[0].getAttribute('innerHTML'),'First name is required');
+            assert.equal(await lastName[0].getAttribute('innerHTML'),'Last name is required');
+            assert.equal(await email[0].getAttribute('innerHTML'),'Email is required');
+            assert.equal(await message[0].getAttribute('innerHTML'),'Message is required');
+
+            console.log("TEST PASSED");
+        } finally {
+            //await driver.quit();
         }
     })
 })
